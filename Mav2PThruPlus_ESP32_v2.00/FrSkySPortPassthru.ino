d463eafa9f7d2e3d81bd487851ce72a666c1dd0b
Eric Stockenstrom <eric@zs6buj.com>
2019-05-07 11:30:29 +0200

    Add ESP32 /driver/uart.h in Libraries folder

--- Mav2PThruPlus_ESP32_v2.00/FrSkySPortPassthru.ino
+++ Mav2PThruPlus_ESP32_v2.00/FrSkySPortPassthru.ino
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <sys/fcntl.h>
-#include <sys/errno.h>
-#include <sys/unistd.h>
-#include <sys/select.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_log.h"
-#include "esp_vfs.h"
-#include "esp_vfs_dev.h"
+//#include <stdio.h>
+//#include <sys/fcntl.h>
+//#include <sys/errno.h>
+//#include <sys/unistd.h>
+//#include <sys/select.h>
+//#include "freertos/FreeRTOS.h"
+//#include "freertos/task.h"
+//#include "esp_log.h"
+//#include "esp_vfs.h"
+//#include "esp_vfs_dev.h"
 #include "driver/uart.h"     
 
 
